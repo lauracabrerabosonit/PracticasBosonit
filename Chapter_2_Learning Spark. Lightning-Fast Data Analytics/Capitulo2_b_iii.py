bedc317ae66c223d542f86ca006dde7a37e24bbc
Laura Cabrera <laura.cabrera@bosonit.com>
2023-03-21 12:01:21 +0100

    Ejercicios Capitulo 2 Spark

---
+++ Chapter_2_Learning Spark. Lightning-Fast Data Analytics/Capitulo2_b_iii.py
@@ -0,0 +1,40 @@
+"""CAPITULO 2 B III"""
+
+from __future__ import print_function
+
+import sys
+
+from pyspark.sql import SparkSession
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: mnmcount <file>", file=sys.stderr)
+        sys.exit(-1)
+
+    spark = (SparkSession
+        .builder
+        .appName("PythonMnMCount")
+        .getOrCreate())
+    # get the M&M data set file name
+    mnm_file = sys.argv[1]
+    # read the file into a Spark DataFrame
+    mnm_df = (spark.read.format("csv")
+        .option("header", "true")
+        .option("inferSchema", "true")
+        .load(mnm_file))
+    mnm_df.show(n=5, truncate=False)
+
+    # aggregate count of all colors and groupBy state and color
+    # orderBy descending order
+    count_mnm_df = (mnm_df.select("State", "Color", "Count")
+                    .groupBy("State", "Color")
+                    .max("Count")
+                    .min("Count")
+                    .orderBy("max(Count)", descending=True))
+
+    # show all the resulting aggregation for all the dates and colors
+    count_mnm_df.show(n=60, truncate=False)
+    print("Total Rows = %d" % (count_mnm_df.count()))
+
+    
+
---
+++ Chapter_2_Learning Spark. Lightning-Fast Data Analytics/MnMcount.scala
@@ -0,0 +1,23 @@
+package main.scala.chapter2
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+/**
+  * Usage: MnMcount <mnm_file_dataset>
+  */
+object MnMcount {
+  def main(args: Array[String]) {
+    val spark = SparkSession
+      .builder
+      .appName("MnMCount")
+      .getOrCreate()
+
+    if (args.length < 1) {
+
+
+    // show the resulting aggregation for California
+    caCountMnNDF.show(10)
+  }
+}
+// scalastyle:on println
